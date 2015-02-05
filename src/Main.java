c062838863bd68ab4862b93d9e67ce18ec5e448d
Guudes <guu.des@gmail.com>
2015-02-05 14:04:07 +0300

    remove unnessesary project files

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         int[] numbers = {10, 5, 3, 9, 4, 0, 3, 9, 4, 0, 3, -5};
         int count = 0;
-
+//
 //        while (true) {
 //            count++;
 //            boolean move = false;
@@ -30,27 +30,27 @@ public class Main {
 //        System.out.print("Количество циклов: " + count);
 
 
-//        for (int i = 0; i < numbers.length - 1; i++) {
-//            int min = numbers[i];
-//            int minPosition = i;
-//            for (int j = i + 1; j < numbers.length; j++) {
-//                if (numbers[j] < min) {
-//                    minPosition = j;
-//                    min = numbers[j];
-//                }
-//            }
-//            if (i != minPosition) {
-//                int tmp = numbers[i];
-//                numbers[i] = numbers[minPosition];
-//                numbers[minPosition] = tmp;
-//            }
-//            count++;
-//        }
-//
-//        for (int i = 0; i < numbers.length; i++) {
-//            System.out.println(numbers[i]);
-//        }
-//
-//        System.out.print("Количество циклов: " + count);
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int min = numbers[i];
+            int minPosition = i;
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] < min) {
+                    minPosition = j;
+                    min = numbers[j];
+                }
+            }
+            if (i != minPosition) {
+                int tmp = numbers[i];
+                numbers[i] = numbers[minPosition];
+                numbers[minPosition] = tmp;
+            }
+            count++;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        System.out.print("Количество циклов: " + count);
     }
 }
