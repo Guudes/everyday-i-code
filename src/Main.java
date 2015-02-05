9277cbf53b2f6a87cb2efd0e2d9aebd57256c779
Guudes <guu.des@gmail.com>
2015-02-05 14:36:40 +0300

    Коммит после мучительных поисков и устранения неполадок

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,22 @@ public class Main {
 
         int[] numbers = {10, 5, 3, 9, 4, 0, 3, 9, 4, 0, 3, -5};
         int count = 0;
-//
-//        while (true) {
-//            count++;
-//            boolean move = false;
-//            for (int i = 0; i < numbers.length - 1; i++) {
-//
-//                if (numbers[i] > numbers[i + 1]) {
-//                    int tmp = numbers[i];
-//                    numbers[i] = numbers[i + 1];
-//                    numbers[i + 1] = tmp;
-//                    move = true;
-//                }
-//            }
-//            if (move == false) {
-//                break;
-//            }
-//        }
-//
-//        for (int i = 0; i < numbers.length; i++) {
-//            System.out.println(numbers[i]);
-//        }
-//
-//        System.out.print("Количество циклов: " + count);
 
+        while (true) {
+            count++;
+            boolean move = false;
+            for (int i = 0; i < numbers.length - 1; i++) {
 
-        for (int i = 0; i < numbers.length - 1; i++) {
-            int min = numbers[i];
-            int minPosition = i;
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[j] < min) {
-                    minPosition = j;
-                    min = numbers[j];
+                if (numbers[i] > numbers[i + 1]) {
+                    int tmp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = tmp;
+                    move = true;
                 }
             }
-            if (i != minPosition) {
-                int tmp = numbers[i];
-                numbers[i] = numbers[minPosition];
-                numbers[minPosition] = tmp;
+            if (move == false) {
+                break;
             }
-            count++;
         }
 
         for (int i = 0; i < numbers.length; i++) {
@@ -52,5 +28,32 @@ public class Main {
         }
 
         System.out.print("Количество циклов: " + count);
+
+
+//        for (int i = 0; i < numbers.length - 1; i++) {
+//            int min = numbers[i];
+//            int minPosition = i;
+//            for (int j = i + 1; j < numbers.length; j++) {
+//                if (numbers[j] < min) {
+//                    minPosition = j;
+//                    min = numbers[j];
+//                }
+//            }
+//            if (i != minPosition) {
+//                int tmp = numbers[i];
+//                numbers[i] = numbers[minPosition];
+//                numbers[minPosition] = tmp;
+//            }
+//            count++;
+//        }
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
+//
+//        System.out.print("Количество циклов: " + count);
+//    }
+
     }
+
 }
