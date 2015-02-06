e1c01293276b3a32dd9df5c13ceee296db718a6f
Guudes <guu.des@gmail.com>
2015-02-07 02:07:11 +0300

    Вынесла методы: stringResultsOut и intResultsOut - вывод данных stringToIntCopy - перенос части массива из String в int
    Установила try/catch для stringResultsOut на случай некорректных данных на входе.

--- src/com/guu/Main.java
+++ src/com/guu/Main.java
@@ -1,57 +1,70 @@
 package com.guu;
 
 import com.guu.tasks.*;
+import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
 
         if (args[0].equalsIgnoreCase("bubbleSort")) {
-
-            int[] bubbleArray = new int[args.length - 1];
-            for (int i = 0; i < bubbleArray.length; i++) {
-                try {
-                    bubbleArray[i] = Integer.parseInt(args[i + 1]);
-                } catch (NumberFormatException e) {
-                    System.err.println("Error: Wrong data type. Enter the number.");
-                    return;
-                }
+            int[] bubbleArray;
+            try {
+                bubbleArray = stringToIntCopy(args);
+            } catch (Exception e) {
+                System.err.println("Error: wrong data type");
+                return;
             }
-
             System.out.println("Sorting with bubble method");
             BubbleSort bubbleSort = new BubbleSort();
             int[] resultBubble = bubbleSort.bubbleSort(bubbleArray);
-            for (int i = 0; i < resultBubble.length; i++) {
-                System.out.print(resultBubble[i] + " ");
-            }
+            intResultOut(resultBubble);
 
         } else if (args[0].equalsIgnoreCase("selectSort")) {
-            System.out.println("Sorting with selection method");
-            int[] selectArray = new int[args.length - 1];
-            for (int i = 0; i < selectArray.length; i++) {
-                selectArray[i] = Integer.parseInt(args[i + 1]);
+            int[] selectArray;
+            try {
+                selectArray = stringToIntCopy(args);
+            } catch (Exception e) {
+                System.err.println("Error: wrong data type");
+                return;
             }
+            System.out.println("Sorting with selection method");
             SelectSort selectSort = new SelectSort();
             int[] resultSelect = selectSort.selectSort(selectArray);
-            for (int i = 0; i < resultSelect.length; i++) {
-                System.out.print(resultSelect[i] + " ");
-            }
+            intResultOut(resultSelect);
 
         } else if (args[0].equalsIgnoreCase("stringLengthSort")) {
+
             System.out.println("Sorting with string length method");
-            String[] stringLengthArray = new String[args.length - 1];
-            for (int i = 0; i < stringLengthArray.length; i++) {
-                stringLengthArray[i] = args[i + 1];
-            }
+            String[] stringLengthArray = Arrays.copyOfRange(args, 1, args.length);
             StringLengthSort stringLengthSort = new StringLengthSort();
             String[] resultStringLength = stringLengthSort.stringLengthSort(stringLengthArray);
-            for (int i = 0; i < resultStringLength.length; i++) {
-                System.out.print(resultStringLength[i] + " ");
-            }
+            stringResultOut(resultStringLength);
 
         } else {
             System.err.println("Error: Unknown sorting method");
         }
+    }
 
+    private static int[] stringToIntCopy(String[] stringArray) {
+        int[] intArray = new int[stringArray.length - 1];
+        for (int i = 0; i < intArray.length; i++) {
+                intArray[i] = Integer.parseInt(stringArray[i + 1]);
+        }
+        return intArray;
+    }
+
+    private static void intResultOut(int[] resultArray) {
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void stringResultOut(String[] resultArray) {
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + " ");
+        }
+        System.out.println();
     }
 }
